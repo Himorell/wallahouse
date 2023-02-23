d2f32ede1d560e9217ecc0b44f843ae4f2ce08a2
Himorell <himorellj@gmail.com>
2023-02-23 19:44:01 +0100

    view routes Api

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,6 @@ use App\Http\Controllers\Api\HouseController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 
 Route::apiResource('houses', HouseController::class);
